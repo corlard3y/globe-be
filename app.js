6f77239ff1a881e1a14288071ba9a9b0e73d61dd
Kolade <corlardey@gmail.com>
2021-10-23 01:27:54 +0100

    route

--- app.js
+++ app.js
@@ -6,11 +6,32 @@ const debug = require('debug')('app')
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+const sessionsRouter = express.Router()
+
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, './public/')))
+// app.use(express.static(path.join(__dirname, './public/')))
+
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+sessionsRouter.route('/').get((req,res)=>{
+    res.render('sessions', {sessions: [
+        {title:'First one', description:'check'},
+        {title:'First one', description:'check'},
+        {title:'First one', description:'check'},
+        {title:'First one', description:'check'},
+        {title:'First one', description:'check'}
+    ]})
+})
+
+sessionsRouter.route('/1').get((req,res)=>{
+    res.send('hello, single sessions')
+})
+
+app.use('/sessions',sessionsRouter)
 
 app.get('/',(req,res) => {
-    res.send('Hello here')
+    res.render('index', {title:'Welcome to Globe!', data: ['Route','Doc','Templates']})
 })
 
 app.listen(PORT, ()=>{
